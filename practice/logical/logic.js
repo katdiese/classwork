454fdc69875a941f2bb6c88917b2ffbe4e9dbbed
Kathy Dieser <katdiese@gmail.com>
2016-01-11 14:55:39 -0700

    worked on html5 audio/video tags

---
+++ practice/logical/logic.js
@@ -0,0 +1,7 @@
+console.log(!("" && 3>5) && true);
+
+console.log((3<5 || false || !(4+5/20*100)) || 1 || !!true);
+
+console.log((10 % 2 && 1 * 100 % 50 && 7 * 12 - 84) && !!(6 - 3 * 2) || !(100 / 2 - 50 + 1) );
+
+console.log((100 === Math.ceil(30 * 3.33)) && !!true || !(false === true) );
