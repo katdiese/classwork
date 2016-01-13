1b6b9988efa49d861fc2f3b5cfdee355cd8b6915
Kathy Dieser <katdiese@gmail.com>
2016-01-13 11:21:10 -0700

    added detailed commenting to arg challenge

--- practice/javascript/arg_challenge/app.js
+++ practice/javascript/arg_challenge/app.js
@@ -11,23 +11,29 @@
 // 	console.log(fakeObject[index]);
 // }
 
+
+//Declared a function myFn with no arguments
 function myFn() {
+	//created conditional if the length of arguments is greater than three,
+	//stop and print to the console "too many arguments", as well as the arguments themselves
 	if(arguments.length > 3) {
 		console.log("AARG! Too many arguments:");
 		console.log(arguments);
 	}
+	//second conditional: if the number of arguments is exactly equal to zero,
+	//stop and log "No arguments!"
 	else if (arguments.length === 0) {
 		console.log("No arguments!");
 	}
 	else {
+		//if both conditions are false  stop and log "Beginning to read arguments"
 		console.log("Beginning to read arguments...");
+		//created for...in loop to check each key in the object argument. If the
+		//index position is true (not undefined), logs "reading argument # ",...., otherwise skip over
 		for(var index in arguments) {
 			if(arguments[index]) {
 				console.log("Reading argument #", parseInt(index)+1, ": ", arguments[index]);
-				// console.log(typeof index);
 			}
-			// console.log(index);
-			// console.log(arguments);
 		}
 	}
 }
