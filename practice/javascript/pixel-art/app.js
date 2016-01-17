5154a06f785a10f993352d2125ad41bc231d5172
Kathy Dieser <katdiese@gmail.com>
2016-01-16 18:57:06 -0700

    added loop to create palette, added all colors of the rainbow

--- practice/javascript/pixel-art/app.js
+++ practice/javascript/pixel-art/app.js
@@ -1,4 +1,4 @@
-var a = document.getElementsByTagName("body")[0];
+var bodyTag = document.getElementsByTagName("body")[0];
 var boxColor;
 
 var css = 'div { height: 1em; width: 1em; border: 1px solid black; float: left; margin: 1px; border-radius: 2px; }',
@@ -14,39 +14,28 @@ if (style.styleSheet){
 
 head.appendChild(style);
 
-var red = document.createElement("div");
-a.appendChild(red);
-red.style.backgroundColor = "red";
-
-red.onclick = function() {
-	boxColor = "red";
-}
-
-var green = document.createElement("div");
-a.appendChild(green);
-green.style.backgroundColor = "green";
-
-green.onclick = function() {
-	boxColor = "green";
-}
-
-var blue = document.createElement("div");
-a.appendChild(blue);
-blue.style.backgroundColor = "blue";
-
-blue.onclick = function() {
-	boxColor = "blue";
-}
+var newDiv;
 
 
 for(i = 0; i <= 1000; i++) {
 	var div = document.createElement("div");
-	a.appendChild(div);
+	bodyTag.appendChild(div);
 	div.onclick = function() {
 		this.style.backgroundColor = boxColor;
 		this.style.border = "none";
 		this.style.margin = "none";
 		this.style.padding = "1px";
+	}
+}
+
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'white'];
+
+for(i = 0; i < colors.length; i++) {
+	newDiv = document.createElement("div");
+	newDiv.style.backgroundColor = colors[i];
+	bodyTag.appendChild(newDiv);
+	newDiv.onclick = function() {
+		boxColor = this.style.backgroundColor;
 	}
 }
 
