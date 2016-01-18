677bba1aaf28d7df6c8f41dbf28b95a61743c102
Kathy Dieser <katdiese@gmail.com>
2016-01-18 16:06:26 -0700

    re-did high-low with peer programming, refactored into jquery

--- practice/javascript/high-low/app.js
+++ practice/javascript/high-low/app.js
@@ -1,35 +1,62 @@
-var randomNum = Math.floor((Math.random() * 100 ) + 1);
+var randomNum = Math.floor((Math.random() * 100) + 1);
 console.log(randomNum);
-console.log(feedback);
 
-var guess = document.getElementById("guess");
-var alreadyGuessed = document.getElementById("guesses");
-console.log(alreadyGuessed);
-
-function compare() {
-	var isNumber = (parseInt(guess.value));
-	function logGuess() {
-		alreadyGuessed.innerHTML = alreadyGuessed.innerHTML + isNumber + ", ";
+function guess(number) {
+	var userNum = $('#number').val();
+	console.log(userNum);
+	if(userNum < randomNum) {
+		alert("number too small");
 	}
-	console.log(isNumber);
-	if(isNumber < randomNum) {
-		// console.log("Too Low!");
-		feedback.innerHTML = "Too low!!";
-		logGuess();
-	}
-	else if(isNumber > randomNum) {
-		// console.log("Too High!");
-		feedback.innerHTML = "Too high!!";
-		logGuess();
-	}
-	else if(isNumber === randomNum) {
-		// console.log("That's it! You win.");
-		feedback.innerHTML = "";
-		alert("You Win!");
+	else if(userNum > randomNum) {
+		alert("number too large");
 	}
 	else {
-		// console.log("Please enter a number between 1 and 100");
-		feedback.innerHTML = "Please enter a number between 1 and 100.";
+		alert("Congrats!");
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+// var randomNum = Math.floor((Math.random() * 100 ) + 1);
+// console.log(randomNum);
+// console.log(feedback);
+
+// var guess = document.getElementById("guess");
+// var alreadyGuessed = document.getElementById("guesses");
+// console.log(alreadyGuessed);
+
+// function compare() {
+// 	var isNumber = (parseInt(guess.value));
+// 	function logGuess() {
+// 		alreadyGuessed.innerHTML = alreadyGuessed.innerHTML + isNumber + ", ";
+// 	}
+// 	console.log(isNumber);
+// 	if(isNumber < randomNum) {
+// 		// console.log("Too Low!");
+// 		feedback.innerHTML = "Too low!!";
+// 		logGuess();
+// 	}
+// 	else if(isNumber > randomNum) {
+// 		// console.log("Too High!");
+// 		feedback.innerHTML = "Too high!!";
+// 		logGuess();
+// 	}
+// 	else if(isNumber === randomNum) {
+// 		// console.log("That's it! You win.");
+// 		feedback.innerHTML = "";
+// 		alert("You Win!");
+// 	}
+// 	else {
+// 		// console.log("Please enter a number between 1 and 100");
+// 		feedback.innerHTML = "Please enter a number between 1 and 100.";
+// 	}
+
+// }
