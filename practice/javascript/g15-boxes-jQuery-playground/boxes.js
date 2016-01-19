89b9009a9ed2d6d7513f214d9a78a926717816c4
Kathy Dieser <katdiese@gmail.com>
2016-01-18 23:02:32 -0700

    added jquery functions to jQuery playground

--- practice/javascript/g15-boxes-jQuery-playground/boxes.js
+++ practice/javascript/g15-boxes-jQuery-playground/boxes.js
@@ -1,9 +1,19 @@
 $(window).load(function() {
 	// alert('ready for DOM manipulation!');
-	$('#secretBox').css('background-color', 'white').append("<h1>secret box!</h1>");
-	$('#row1').children("div").addClass("boxType3");
-	$('#row4 div:last-child').css('display','none');
-	$('.boxType1').css('background-color', 'white');
-	$('#row2 div').filter(':nth-child(1), :nth-child(2)').removeClass('box');
-	$('div').not(".row, #secretBox").css('width', '2px');
+	// $('#secretBox').css('background-color', 'white').append("<h1>secret box!</h1>");
+	// $('#row1').children("div").addClass("boxType3");
+	// $('#row4 div:last-child').css('display','none');
+	// $('.boxType1').css('background-color', 'white');
+	// $('#row2 div').filter(':nth-child(1), :nth-child(2)').removeClass('box');
+	// $('div:not(.row, #secretBox)').css('width', '2px');
+  $('a').click(function (e) {
+    alert("You can never leave!");
+    e.preventDefault();
+  });
+  $('div.box').click(function() {
+    $(this).toggleClass('puppy');
+  });
+  $('container').click(function() {
+    $(this).toggleClass('blackBackground');
+  });
 });
