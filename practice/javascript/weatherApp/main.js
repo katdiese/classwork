379be192732a2500bc82388a915163e3a2661f33
Kathy Dieser <katdiese@gmail.com>
2016-01-21 14:12:41 -0700

    accessed API's to collect location and weather data

--- practice/javascript/weatherApp/main.js
+++ practice/javascript/weatherApp/main.js
@@ -1,24 +1,43 @@
 $(document).ready(function() {
-  $(function() {
-    $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
-      function(json) {
-        $("#ip").text("My IP address is: " + json.ip);
+  $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
+    function(json) {
+      $("#ip").text("My IP address is: " + json.ip);
+    });
+//   var settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": "http://freegeoip.net/json/",
+//   "method": "GET"
+// }
 
-      }
-    );
+// $.ajax(settings).done(function (response) {
+//   $('#latitude').text('My Latitude: ' + response['latitude']);
+//   $('#longitude').text('My Longitude: ' + response['longitude']);
+//   console.log(response['longitude']);
+// });
+
+var myFn = function() {
+  var cityName = $('input[name="location"]').val();
+  var settingsWeather = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=3c438ab921656e7189ededb4dbb9bb82",
+    "method": "GET"
+  }
+  $.ajax(settingsWeather).done(function (response) {
+    $('#comment').text(response.weather[0].description);
+    console.log(response);
+    $('#latitude').text('Latitude: ' + response.coord.lat);
+    $('#longitude').text('Longitude: ' + response.coord.lon);
   });
-  var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "http://freegeoip.net/json/",
-  "method": "GET"
 }
-
-$.ajax(settings).done(function (response) {
-  $('#latitude').text('Latitude: ' + response['latitude']);
-  $('#longitude').text('Longitude: ' + response['longitude']);
-  console.log(response['longitude']);
+myFn();
+$('#submit').click(function() {
+  myFn();
 });
 
-
 });
+
+// document ready
+  // myFn()
+  // $('#submit').on('click', myFn)
